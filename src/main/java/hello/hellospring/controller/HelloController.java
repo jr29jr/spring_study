98382f63cf4f8023cb05cfc112fbe0d908fc73fa
jr29jr <jr30jr@naver.com>
2023-02-10 17:34:18 +0900

    api test

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -13,9 +14,42 @@ public class HelloController {
         return "hello";
     }
 
-    @GetMapping("hello-mve")
-    public String helloMve(@RequestParam("name") String name, Model model){
+    @GetMapping("hello-mvc")
+    public String helloMve(@RequestParam( value ="name", required = false) String name, Model model){
         model.addAttribute("name",name);
-        return "hello template";
+        return "hello-template";
+    }
+
+    /**
+     * 응답 body에 직접 넣겠다.
+     * @param name
+     * @return
+     */
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name")String name){
+        return "hello "+ name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello  helloApi(@RequestParam("name")String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        //json으로 자동으로 리턴하네?
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
     }
 }
